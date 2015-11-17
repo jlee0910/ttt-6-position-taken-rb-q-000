5ed38fcdfb6ad6623ea538e5091b6eb15f688cec
jlee0910 <jlee0910@gmail.com>
2015-11-17 14:02:47 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,27 @@
 # code your #position_taken? method here!
-def position_taken?(board, position)
- board[position] != " "
- position == nil 
- if board[position] == "X" || board[position] == "O"
-      return true
-   else
-      return false
-   end
- end
+#def position_taken?(board, position)
+ #board[position] != " "
+ #position == nil 
+ #if board[position] == "X" || board[position] == "O"
+  #    return true
+   #else
+    #  return false
+   #end
+ #end 
+
+ def position_taken?(board, position)
+  # basic solution: 
+
+  # taken = nil
+  # if (board[position] ==  " " || board[position] == "" || board[position] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+  (board[position] ==  " " || board[position] == "" || board[position] == nil) ? 
+    false : true
+
+end
